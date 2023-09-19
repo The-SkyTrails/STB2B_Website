627a43e77752835f00b464fdde7c2fe191f9f712
Qamar Ali <qamarali205@gmail.com>
2023-09-19 10:08:39 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,45 +5,15 @@ import "./Header.css";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Fade from "@mui/material/Fade";
-import { Box, Button, Typography, Paper, makeStyles } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import STLOGO from "../Images/ST-Main-Logo.png";
 import { useDispatch, useSelector, useReducer } from "react-redux";
 import { logoutAction } from "../Redux/Auth/logIn/actionLogin";
-import Modal from "@mui/material/Modal";
-import CloseIcon from "@mui/icons-material/Close";
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import axios from "axios";
-
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  FormHelperText,
-} from "@chakra-ui/react";
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
 
 const Header = () => {
   const [scrollYvalue, setScrollYValue] = useState(0);
   const reducerState = useSelector((state) => state);
-  const [openModal, setOpenModal] = React.useState(false);
-  const [amount, setAmount] = React.useState("");
-  const handleOpenModal = () => setOpenModal(true);
-  const handleCloseModal = () => setOpenModal(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const openLoginpage = () => {
@@ -78,217 +48,94 @@ const Header = () => {
 
     return () => window.removeEventListener("scroll", updateSrollYPosition);
   });
-
-  const handleRazorpay = (data) => {
-    console.log("handleRazorpay called");
-    const options = {
-      key: "rzp_test_rSxJ8wZCLzTJck",
-      amount: amount * 100,
-      currency: "INR",
-      name: "The SkyTrails",
-      description: "Test Transaction",
-      image: STLOGO,
-      order_id: data.id,
-      handler: function (response) {
-        console.log(response);
-      },
-    };
-    const rzp = new window.Razorpay(options);
-    rzp.open();
-  };
-  const handlePayment = (e) => {
-    e.preventDefault();
-    const data = { amount: amount };
-    axios
-      .post("http://localhost:8000/travvolt/wallet/rechargeWallet", data)
-      .then((res) => {
-        console.log(res.data);
-        handleRazorpay(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    alert(amount);
-    setAmount("");
-    handleCloseModal();
-  };
   return (
-    
-    
-    <div className={scrollYvalue > 45 ? "header_scroll" : "header"} >
-    
-    <div >
-      <a href="/">
-        <img
-          src={STLOGO}
-          style={{ width: "90%", height: "70px" }}
-          className="mt-2 ms-2"
-          alt="logo"
-        />
-      </a>
-    </div>
-  
-    <div className="welcome">
-      <p>Contect Your Representative</p>
-      <p className="welPrice">
-        Cash Balance: ₹ {reducerState?.logIn?.loginData?.data?.data?.balance}
-      </p>
-      <button  onClick={handleOpenModal}>Recharge</button>
-  
-      <div style={{ marginBottom: "25px", height: "60px" }}>
-        <Box marginTop={3}>
-          <Typography
-            sx={{
-              color: "#2525250",
-              fontSize: "15px",
-              fontWeight: "bold",
-              textAlign: "center",
-            }}
-          >
-            Welcome{" "}
-          </Typography>
-          <Typography
-            sx={{
-              color: "#0052D0",
-              fontSize: "22px",
-              fontWeight: "bold",
-              textAlign: "center",
-            }}
-          >
-            {reducerState?.logIn?.loginData?.data?.data?.username}
-          </Typography>
-        </Box>
-      </div>
-  
-      <div
-        style={{
-          marginLeft: "-25px",
-        }}
-      >
-        <ArrowDropDownIcon
-          onClick={handleClick}
-          id="menu"
-          aria-controls={open ? "menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-          cursor="pointer"
-        />
-        <Menu
-          id="menu"
-          MenuListProps={{
-            "aria-labelledby": "fade-button",
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          TransitionComponent={Fade}
-        >
-          <MenuItem onClick={handleSubmit}>
-            {reducerState?.logIn?.loginData?.data?.data ? "Logout" : "Login"}
-          </MenuItem>
-          <MenuItem onClick={editPackage}>My Package</MenuItem>
-        </Menu>
-      </div>
-    </div>
-  
-    <Modal
-      open={openModal}
-      onClose={handleCloseModal}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
+    <div
+      className="header"
+      style={
+        scrollYvalue > 45
+          ? {
+              position: "fixed",
+              top: "1px",
+              left: "95px",
+              width: "85%",
+              marginTop: "0px",
+              zIndex: "10",
+              backgroundColor: "#E5E4E2",
+            }
+          : {}
+      }
     >
-      <Box sx={style}>
-        <Typography
-          id="modal-modal-title"
-          variant="h6"
-          component="h2"
+      <div>
+        <a href="/">
+          <img
+            src={STLOGO}
+            style={{ width: "200px", height: "70px" }}
+            className="mt-2 ms-2"
+          />
+        </a>
+      </div>
+
+      <div className="welcome">
+        <p>Contect Your Representative</p>
+        <p className="welPrice">
+          Cash Balance: ₹ {reducerState?.logIn?.loginData?.data?.data?.balance}
+        </p>
+        <p>Recharge</p>
+
+        <div style={{ marginBottom: "15px" }}>
+          <Box marginTop={3}>
+            <Typography
+              sx={{
+                color: "#2525250",
+                fontSize: "15px",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              Welcome{" "}
+            </Typography>
+            <Typography
+              sx={{
+                color: "#0052D0",
+                fontSize: "22px",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              {reducerState?.logIn?.loginData?.data?.data?.username}
+            </Typography>
+          </Box>
+        </div>
+        <div
           style={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: "-1rem",
+            marginLeft: "-25px",
           }}
         >
-          <Typography
-            variant="h6"
-            component="h2"
-            style={{
-              cursor: "pointer",
-              textDecoration: "none", // Remove underline by default
-              transition: "text-decoration 0.3s ease", // Smooth transition
-            }}
-          >
-            Enter Payment Detail
-          </Typography>
-  
-          <CloseIcon
-            onClick={handleCloseModal}
-            style={{ cursor: "pointer" }}
+          <ArrowDropDownIcon
+            onClick={handleClick}
+            id="menu"
+            aria-controls={open ? "menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            cursor="pointer"
           />
-        </Typography>
-  
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-  
-          <Paper elevation={3} style={{ padding: '20px', maxWidth: '400px', margin: 'auto' }}>
-            <form onSubmit={handlePayment}>
-  
-              <FormControl fullWidth sx={{ marginBottom: 2 }}>
-                <FormLabel>Name</FormLabel>
-                <OutlinedInput
-                  type="text"
-                  value={reducerState?.logIn?.loginData?.data?.data?.username || ''}
-                  readOnly
-                  sx={{ width: '100%' }}
-                />
-              </FormControl>
-  
-  
-              <FormControl fullWidth sx={{ marginBottom: 2 }}>
-                <FormLabel>Email address</FormLabel>
-                <OutlinedInput
-                  type="email"
-                  value={reducerState?.logIn?.loginData?.data?.data?.email || ''}
-                  readOnly
-                  placeholder="Enter your email"
-                  sx={{ width: '100%' }}
-                />
-              </FormControl>
-  
-              <FormControl fullWidth sx={{ marginBottom: 6 }}>
-                <FormLabel>Amount</FormLabel>
-                <OutlinedInput
-  
-                  startAdornment={<InputAdornment position="start">₹</InputAdornment>}
-  
-                  type="text"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                  sx={{ width: '100%' }}
-                />
-              </FormControl>
-  
-              <Box display="flex" justifyContent="space-between">
-                <Button variant="contained" type="submit" sx={{ margin: 0.10 }}>
-                    Recharge Wallet
-                </Button>
-                <Button variant="contained"
-  
-                  onClick={handleCloseModal}
-                  sx={{ margin: 0.5, backgroundColor: 'red', margin: 0.10 }}>
-                  Cancel
-                </Button>
-  
-              </Box>
-            </form>
-          </Paper>
-  
-        </Typography>
-      </Box>
-    </Modal>
-  </div>
-  
-  
+          <Menu
+            id="menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem onClick={handleSubmit}>Logout</MenuItem>
+            <MenuItem onClick={editPackage}>My Package</MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </div>
   );
-}; 
+};
 
 export default Header;
